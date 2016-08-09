71f4fc2a2c8d935765dd8f4fd8e3334e4a71f070
System administrator <root@madoka.brage.info>
2016-08-09 16:56:37 +0100

    Bumpety

--- configuration.nix
+++ configuration.nix
@@ -4,19 +4,7 @@
 
 { config, pkgs, lib, ... }:
 
-let sshKeys = {
-  svein = [ "cert-authority ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC1YduKCwNp6O92ZedMZycjdDJNmBAY1zyaRBramnXsjBG0d75c37KXoaGPfdcGSsLJ9fxyv+CONsMj1c/sukTiLXMu0Rln62XeWo0dncX4FRXhJ7ShRdTFKwt0Kx5zzpBjTjw1ZOof3gGQVumLQKM9LCjTSFYSWyjPSxDHiV88dL1jt03qp0L3L8GjXwF5onUe3yun0+Cqj9CSVeN+I0cYprlpPucpCWIsrgclYHEcPg67UN3RVwxH/FV+sGAerJyoJFo7/Qu9MoTPP+eRFH8yGuoPforZPZTxUFIjkH2bYMozo1kcf3RQJfrKTTMa1mDfil9THeWP6E+iADSBDh4HLWcEkd3kGFuY5fhaRVFABFCqDsS7PXho+qg34PKfjn/2yuSwA4t4DPU6/T7JuF2rZs+WQX2I45WVvhtk43GNTk4dQTyfcxz9erryEDWVVxsrYoUQnSOVtooz/nJN4jK0xJNGoK0ZVwp/zlLhnrdrxO9GqYSABSTkedNuIoPrxDda0n8kBCtTryOPZzYBmBg6moLy9MCh7Q3TuFJMtMHLIlm3GChXeMwlWm2cYPu0vu8//HLhiOg9XIeLbLaFmOZA2i3TzksoevuNUMr+jXReVUGqcVoeejwz4ajII+swIgbUMWPIUaz4YDjtjG58B6N8r1OaeKFG/KMA6joubAuSPQ== svein@tsugumi" ];
-  bloxgate = [
-    "ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAkkA8U0Mw14OM+FUKLDaK3CmZtZyyQS7hBcMcXAUpvzldCJJXN59JDMnrEEvdQKPwaaRb6uWPOet4uE+C2RmB+MxpHvQYqZdbgo743HiacXtkuqi/85NyEOATCapJt0vpYYx9kqi8kyp5liiLcr3TbzDKAf3JJqG671/ZMUNlZvUh3tHD7An3mrEHSl7Zh2EETKuimpEbtO5cVikhFWndwugVmKsCMLwpaMcRGuAnuVGvuoD0XEz8X8W9k/+FM2lott4mAg8npo+ZXhbKZXil3uuUc8ln8loCwpZvg+CzdehWr/JcClofwOfUrLwO7EdvIu6Mu3c5pe0utubWuQgGZw== bloxgate-new-2016"
-    "ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAy/+H1vDZGfsRQJJkZJXDSRUAoQsw+K8P8nJj2CKugl+vw0+un2DPDJRi4GszqC5jkjKVNFHQxKtUZGMGBq8IzxXVudgdMQ0MYoQLxi563UMuPJ+94yuYvUZZ4lCzsq7lUZ6StwfoygQ/P2l6mVXhyCib7CYgS2ubstOn6H9A5UFR4FtZus7IGtpdUzMjzbjs8LQpS+Vr4Ju9cgGGmcKGWV3llz9zq+UDPSP2MZs3phvfo0nPSHTYD6m8tH7lGw945ZTrOj6z5LaHSZZuXqX9NxgdB7WiSYru9iZQVU7wDfZmnkeQ1X3IQBMI+LlqN9YqP6MjAjmQDv3FpmjFY3dQtQ== east-key"
-  ];
-  darqen27 = [ "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDfVJLC3cT5YPuwzvFF1BVLXod5v8q+zgaroAuMpUhZ9vML/R4c8+6Y9td5k7s2FAOfnKVCMrrYAzJWMReyqvUCsg1ZZViRhJtV2q5G66K69wdgXaacgD89S/p51ek4HsqzgwfeUtknDcwJJ9ZlTRhdVLmTc06mtChMC0QO3b5gUVM6hXS7lZivlql+SXOe3KaMNABlUpOMbEzcArSzde5suJyH2fc8OLBf1bhEt6xBaxQykNs1HJshu1UHjwGVCOkTFvQ51ohZbFdNHM2B1Pr1QF08ad9kCflWGjnXpBgXMwEpzLv4P1wOaRaFJn1bQjtTVGgyFxyEhjMJer5nexmA5rLTS9yKYlJ85nwJ6fmKWq3lcMkAmDlSXCbR047N6p5n4gBHnPcoBSfGgoX1RvsYdFW2Fa+lYyjJj40cWdaaCP0NfD7oAg08G3For/eaUUjJz1wtwFtaFEFyEu8hbll5MnOEosLcC5aa24Av7Fxz7P5YDVvPKUNn5HEsgxQf8X4pnUC69irdWvxoNsY5UeHj0R4YBKVHr9kvYPoeGr+5mftLtSeqzHHR5B7jaUL1kCsgu7Z/ATmoetaLWuQ63ToPF7a9P5QhC0K2dzmkXWWnmPr65cqy+h9YgyOQK3tMciOQKPQ3VSVTWUoovkJ/z9+Ns+KV2vQldI/VYfyjXTdgPw== darqen27@icloud.com" ];
-  jmc = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIZHmzn8nW1NUliqqB+4db2hV0jS4A7RrZ+2/QcLwsC2 jmc@clockwerk" ];
-  kim = [ "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAIEA3j9muBMkqIAQ8BLBK5Ki4I1l2gg//Yt/YLmZd6nAaqYO4OeZ50k7x4F1OFRnyWScDqb4C5XggG8FaBQVe5RfP43sKDFx6F9En/zPB0JwbWT7iVXlZHFLLqqZ+vzrEmEYexQSwftpR1neKWb39fZjOcZTvd7Tk3sGNbnr/0LMYW0= kim@localhost" ];
-  buizerd = [ "ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAhD/TX8mV0yCbuVsSE8Nno9YLBIMccUKmmEK4Ps1z77D+mi9F6HeaLFJ9FGIvhZ1BVChCJrNTNsTSqRcHLoDye3IsQa080mwhK2hRYPpKPhpa8/Y1zXP6bk6oePVeZDHR1tMgkRJzM/8kOpgNKRKIcdtFU7KWvCWywAeLi8BjrLE3fHU1a2I3ZrT4FUintgjtYVPD7p3m7AEsx4nvqOCxHqlt04i175bwvQ4HVgFzNYgQX9oQw4NgPfDsCCNXkDcVBwZNakUu8q9guHbjWO+1IXvUwfTUEk3pRpbM3glbWzba2PXJA+xM4/NYhZbfXqsXY8vMOmkC0Z54gVEN07s8lw== buizerd@localhost" ];
-  luke = [ "ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAgEClqIZgCTdObRDyG/0x+/72Ugbv1tqDjBIdmWbq4htd9I4WP2SWMh5r73L0DUVqs6HYS2JusFN8NSFDKEIr19LPuirrmq/TcdJBeEN25IMZSs2xId5gjhxxp/NloNyh6tQ4ketpi8cyWz2KqHX5wHjk52k2QOQ461jY/qZJ1eqDkwXA1Fxm0CbJw6HkfFhDLfEffsev2Duei/3M7joy5NxATZTeKZ/BG9yCEG9/oDsR3i3PUssMtZ2D98bLqTpU0i+WuSXZDZHZTd8nRv6f7ZndFGCldoQcSc4g/QH7qP40xjzN2jVeolQ5dI8cAhb1JeykRURskcA9GPkCb4HhBRHHuPN+w1rEbFSV1APsxjOV5T2628om78VX37cwrFT/lP6HP9f31B6oXu9hUTGNRnSI6bxKAF3TmqWsZgffHZ84wInW2sGV4lIAYKiMbnd/DanxsIkqbzWHigAmj04tSi3nbB6Jm+qtLg0TPkn29KFQW7kCwAZ7qKTmaWocbzWtyvA8Y1TNlzMJfT0vQ9dRh3onvPVLZwcVI/4dUFrGhJGREHJJ/Po1ZFry3ugE7rJ5FplrVC4AmJLWliVXacDvn+zbwIZHxbzUY/+xuPphvSq+kXoi6yjxTSYRnm1Xl6MAHF817kMypE+DYafCItxvJHklAdDc5zoXQ6p9I8BrTevsAU= castone22@gmail.com" ];
-  vindex = [ "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCoFU4rmBlaVsQj/odpzWYtBu4j4TDQpNTTpkAIxQ+Qu/uikMkdq/CrSERgE+fPd6KLHz7nBCA5wIGoP0GDOCF/OShyP4F9KoQeCcuT1s2+rQBpRG3wjOftPil8V6Rw3dl5lLFcmyKDtNUpTQupenyH+nzlmBIJ9OPB5y7CIxulk+MgdA8dHF9IROGnfevJe4SkMdJixu4lgaGSx7JHztnq1RRN86FCIhtrc4qBJfbSwcrL3r3yGOK+trdXt2WAf1smXsskTmEAhpUnFrY5t74g9zfzmDJapnpxpUmAEY1N5Xg4xICAEOzma4Gi1CT8NRvUfsb/IkPUGfartASvRVbB" ];  
-};
+let sshKeys = import ./sshKeys.nix;
 in
 
 {
@@ -67,6 +55,9 @@ in
   networking.firewall = {
     allowPing = true;
     allowedTCPPorts = [ 80 443 25565 25566 4000 12345 ];
+    allowedUDPPorts = [
+      34197 # Factorio
+    ];
   };
   networking.firewall.allowedUDPPortRanges = [{from = 60000; to = 61000;}];
 
@@ -164,12 +155,16 @@ in
     isNormalUser = true; 
     uid = 1009;
   };
-
   users.extraUsers.vindex = {
     isNormalUser = true; 
     uid = 1011;
     extraGroups = [ ];
     openssh.authorizedKeys.keys=sshKeys.vindex;
+  };
+  users.extraUsers.xgas = {
+    isNormalUser = true;
+    uid = 1012;
+    openssh.authorizedKeys.keys = sshKeys.xgas;
   };
 
 
@@ -217,12 +212,34 @@ in
   nix.gc.dates = "Thu 03:15";
   nix.gc.options = "--delete-older-than 14d";
   environment.systemPackages = with pkgs; [
-    tcpdump psmisc atop gdb stack wget file zip mosh irssi links screen telnet unison
+    tcpdump psmisc gdb stack wget file zip irssi links telnet unison
     git mutt openjdk unzip imagemagick parallel moreutils vim nix-repl whois
-    znc bsdgames shared_mime_info
+    znc bsdgames shared_mime_info nox emacs24-nox hdparm nmap sysstat 
+    screen atop
   ];
   programs.zsh.enable = true;
+  programs.mosh.enable = true;
   nixpkgs.config.allowUnfree = true;
+
+#  zramSwap.enable = true;
+
+  # VPN
+  services.openvpn = {
+    servers = {
+      server = {
+        config = ''
+          dev tun
+          ifconfig 10.8.0.1 10.8.0.2
+          secret /root/static.key
+          comp-lzo
+        '';
+        up = "ip route add ...";
+        down = "ip route del ...";
+	autoStart = true;
+      };
+    };
+    
+  };
 
   # Workaround for inpure's server.
   networking.extraHosts = ''
